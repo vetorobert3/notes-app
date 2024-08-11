b9ec1cf0303ab2930de53be65bb0a62b44be1688
Veto Ramirez <vetoroert3@gmail.com>
2024-08-11 09:40:15 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const chalk = require('chalk') 
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('17.7.2')
+
+// Create add command
+yargs.command({
+    command: 'add', 
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    describe: 'Add a new note',
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: "Remove note", 
+    handler: function () {
+        console.log('Removing note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: "List all notes", 
+    handler: function () {
+        console.log('Listing notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: "Read note", 
+    handler: function () {
+        console.log('Reading note!')
+    }
+})
+
+// add, remove, read, and list notes
+
+yargs.parse()
